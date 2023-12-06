9ff1cdbee15ffa58826121d4e3764815c6e2bab8
nkanyi73 <nkanyi73@gmail.com>
2023-12-06 08:56:07 +0000

    Add voices based on dialogue script to NPCs

---
+++ Assets/Scripts/RandomNavMeshPosition.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomNavMeshPosition : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Animator npcAnimator;
+
+    private Vector3 targetPosition;
+
+    private float _waitTime;
+    private float _waitTimer;
+    public float wanderDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _waitTime = 5f;
+        npcAnimator.Play("Thriller Part 3");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (agent.speed > 1)
+        //{
+        //    walkinginganimation
+        //    npcAnimator.Play("Walking");
+        //}
+        //else
+        //{
+        //    idleanimation
+        //    npcAnimator.Play("Thriller Part 3");
+
+        //}
+
+        if (agent.enabled == false)
+            return;
+
+        if (_waitTimer < _waitTime)
+        {
+            _waitTimer += Time.deltaTime;
+        }
+        else
+        {
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * wanderDistance;
+
+            randomDirection += transform.position;
+
+            NavMeshHit navHit;
+
+            NavMesh.SamplePosition(randomDirection, out navHit, wanderDistance, NavMesh.AllAreas);
+
+            targetPosition = navHit.position;
+
+            agent.speed = 1.0f;
+
+            agent.SetDestination(targetPosition);
+
+            _waitTimer = 0f;
+        }
+    }
+}
